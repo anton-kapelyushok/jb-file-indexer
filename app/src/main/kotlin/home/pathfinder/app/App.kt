ac17bda494f2f021122a2d92aab1f848d1f66876
Anton Kapelyushok <amtonk@gmail.com>
2021-10-22 16:14:40 +0300

    done?

--- app/src/main/kotlin/home/pathfinder/app/App.kt
+++ app/src/main/kotlin/home/pathfinder/app/App.kt
@@ -2,10 +2,12 @@ package home.pathfinder.app
 
 import home.pathfinder.indexing.fileIndexer
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlin.system.measureTimeMillis
+import kotlinx.coroutines.selects.select
+import kotlinx.coroutines.withContext
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
 
@@ -16,49 +18,99 @@ fun main() {
 //        val fileIndexer = fileIndexer { path ->
 //            flow { emit(Posting(path.split("/").last(), 0)) }
 //        }
+
         val fileIndexer = fileIndexer()
 
-        launch(Dispatchers.Default) { fileIndexer.go(this) }
+        val indexerJob = launch(Dispatchers.Default) { fileIndexer.go(this) }
 
-        fileIndexer.updateContentRoots(setOf("."))
-        val millis = measureTimeMillis { println(fileIndexer.searchExact("override").toList()) }
-        println("Started in $millis")
+        val requestInput = Channel<Unit>()
+        val consoleInput = Channel<String>()
+        val consoleReaderJob = launch(Dispatchers.IO) {
+            for (req in requestInput) {
+                val read = readLine()
+                if (read == null) {
+                    consoleInput.close()
+                    break
+                }
+                consoleInput.send(read)
+            }
+            consoleInput.close()
+        }
 
-
-        launch(Dispatchers.IO) {
-            while (true) {
-                println("enter command")
-                val term = readLine()!!
-                println()
-
-                val parts = term.split(" ").map { it.trim() }
-
-                when (val cmd = parts[0]) {
-                    "watch" -> try {
-                        fileIndexer.updateContentRoots(parts.subList(1, parts.size).toSet())
-                    } catch (e: Throwable) {
-                        println("error: $e")
+        try {
+            withContext(Dispatchers.IO) {
+                var inputRequested = false
+                suspend fun readLine(): String {
+                    if (!inputRequested) {
+                        requestInput.send(Unit)
                     }
-                    "find" -> {
-                        launch {
-                            val (value, duration) = measureTimedValue {
-                                if (parts.size != 2) return@measureTimedValue emptyList<Any>()
-                                try {
+                    inputRequested = true
+                    return consoleInput.receive().also { inputRequested = false }
+                }
+
+                while (true) {
+                    println("Enter command (watch/find/status/exit)")
+
+                    val input = readLine()
+                    println()
+
+                    val parts = input.split(" ").map { it.trim() }
+
+                    when (val cmd = parts[0]) {
+
+                        "watch" -> try {
+                            fileIndexer.updateContentRoots(parts.subList(1, parts.size).toSet())
+                        } catch (e: Throwable) {
+                            println("error: $e")
+                        }
+
+                        "find" -> {
+                            println("searching, press any key to cancel")
+                            val searchJob = launch {
+                                val (result, duration) = measureTimedValue {
+                                    if (parts.size != 2) return@measureTimedValue emptyList()
                                     fileIndexer.searchExact(parts[1]).toList()
-                                } catch (e: Throwable) {
-                                    println("launch $e")
+                                }
+                                if (result.isEmpty()) println("Nothing found")
+                                else {
+                                    result.forEach { (documentName, term, lineNumber) ->
+                                        println("$term: $documentName:$lineNumber")
+                                    }
+                                    println()
+                                    println("Total ${result.size} entries")
+                                }
+                                println("Found in ${duration.inWholeMilliseconds}ms")
+                                println()
+                            }
+
+                            val readlineJob = launch { readLine() }
+                            select<Unit> {
+                                readlineJob.onJoin {
+                                    searchJob.cancel()
+                                }
+                                searchJob.onJoin {
+                                    readlineJob.cancel()
                                 }
                             }
-                            println(value)
-                            println("Found in ${duration.inWholeMilliseconds}ms")
+
+                            searchJob.join()
                         }
+
+                        "status" -> {
+                            println(fileIndexer.state.value)
+                            println()
+                        }
+                        "exit" -> {
+                            break
+                        }
+                        else -> println("unknown cmd $cmd")
                     }
-                    "status" -> {
-                        println(fileIndexer.state.value)
-                    }
-                    else -> println("unknown cmd $cmd")
                 }
             }
+        } finally {
+            requestInput.cancel()
+            indexerJob.cancel()
+            consoleReaderJob.cancel()
         }
     }
 }
