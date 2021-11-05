0ebc295eadee4b3d35d22352710cc6758a199b97
Anton Kapelyushok <amtonk@gmail.com>
2021-11-05 16:49:50 +0300

    File Indexer Performance test

---
+++ indexer/src/test/kotlin/home/pathfinder/indexing/FileIndexerPerformanceTest.kt
@@ -0,0 +1,51 @@
+package home.pathfinder.indexing
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import kotlin.system.measureTimeMillis
+
+private val workingDirRoot = "/Users/akapelyushok/Projects/intellij-community"
+
+class FileIndexerPerformanceTest {
+    @Test
+    @Disabled
+    fun `time until first search`() {
+        runBlocking {
+            val indexer = fileIndexer(
+                segmentedIndex(mergeSegmentsConcurrency = 1),
+                additionalProperties = mapOf(rwInitialEmitFromFileHasherHackDisabled to false)
+            )
+            val job = launch(Dispatchers.Default) { indexer.go(this) }
+
+            try {
+                indexer.updateContentRoots(setOf(), setOf())
+                val millis = measureTimeMillis {
+                    val statusJob = launch {
+                        var seconds = 0
+                        while (true) {
+                            println("#### secondsElapsed: $seconds #####")
+                            println(indexer.state.value)
+                            println("#############################")
+                            println()
+                            delay(1000)
+                            seconds++
+                        }
+                    }
+                    indexer.updateContentRoots(setOf(workingDirRoot), setOf())
+                    indexer.searchExact("poupa").collect()
+                    statusJob.cancel()
+                }
+                println("Completed in $millis millis")
+
+            } finally {
+                job.cancel()
+            }
+        }
+    }
+
+}
